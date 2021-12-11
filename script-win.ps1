b8ab49afc1b6d1558c73de8f94e41f778ba47d57
Anant Kaul <56610952+anantkaul@users.noreply.github.com>
2021-12-11 15:02:53 +0530

    Done with Alpha Testing
    Fixed all bugs ...
    All alpha testings working fine on a fresh windows ...n

--- script-win.ps1
+++ script-win.ps1
@@ -17,12 +17,12 @@ function yellow {
 
 if ($setup -eq "y" -or $setup -eq "Y" -or $setup -eq "yes" -or $setup -eq "YES") {
    # Installing Invoke-Atomic Framework
-   Write-Output "`n >> Installing Invoke-Atomic Framework ..."
+   Write-Output "`n >> Installing Invoke-Atomic Framework ..." | yellow
    Install-PackageProvider -Name NuGet -Scope CurrentUser -Force
    Install-Module -Name invoke-atomicredteam,powershell-yaml -Scope CurrentUser -Force
-   Write-Output " >> Installed Invoke-Atomic Framework successfully ..." | green
+   Write-Output "`n >> Successfully Installed Invoke-Atomic Framework ..." | green
    # Getting the atomics
-   Write-Output "`n >> Getting the atomics ...`n"
+   Write-Output "`n >> Getting the atomics ...`n" | yellow
    Invoke-Expression (Invoke-WebRequest 'https://raw.githubusercontent.com/redcanaryco/invoke-atomicredteam/master/install-atomicredteam.ps1' -UseBasicParsing); Install-AtomicRedTeam -getAtomics -Force
    Import-Module "C:\AtomicRedTeam\invoke-atomicredteam\Invoke-AtomicRedTeam.psd1" -Force
 }
